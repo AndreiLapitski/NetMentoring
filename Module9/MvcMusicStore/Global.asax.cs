be2cb1fa5e6e76a9c4962535e35d8e5e0b3b7302
Andrei Lapitski2 <Andrei_Lapitski2@epam.com>
2019-05-24 09:57:47 +0300

    add Module9

---
+++ Module9/MvcMusicStore/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using MvcMusicStore.Controllers;
+using MvcMusicStore.Monitoring;
+using NLog;
+using PerformanceCounterHelper;
+
+namespace MvcMusicStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private readonly ILogger logger;
+
+        public MvcApplication()
+        {
+            logger = LogManager.GetCurrentClassLogger();
+        }
+
+        protected void Application_Start()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(HomeController).Assembly);
+            builder.Register(l => LogManager.GetLogger("ForControllers")).As<ILogger>();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(builder.Build()));
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            using (CounterHelper<PerformanceCounter> counterHelper = 
+                PerformanceHelper.CreateCounterHelper<PerformanceCounter>("Test project"))
+            {
+                counterHelper.RawValue(PerformanceCounter.LogInCounter, 0);
+            }
+        }
+
+        protected void Application_Error()
+        {
+            var e = Server.GetLastError();
+            logger.Error(e.Message);
+        }
+    }
+}
---
+++ Module9/MvcMusicStore/Monitoring/PerformanceCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using PerformanceCounterHelper;
+
+namespace MvcMusicStore.Monitoring
+{
+    [PerformanceCounterCategory("MvcPerformanceCounter", PerformanceCounterCategoryType.SingleInstance, "Set of counters for MvcMusicStore application")]
+    public enum PerformanceCounter
+    {
+        [PerformanceCounter("LogInCount", "Log In success count", PerformanceCounterType.NumberOfItems32)]
+        LogInCounter = 1,
+
+        [PerformanceCounter("LogOffCount", "Log Off success count", PerformanceCounterType.NumberOfItems32)]
+        LogOffCounter = 2,
+    }
+}
